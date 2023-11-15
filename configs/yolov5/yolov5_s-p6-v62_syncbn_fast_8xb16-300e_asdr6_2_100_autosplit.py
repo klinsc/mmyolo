22dcd55a0194675ce7beb9cb3f32e6d1c501e427
klinsc <klinscea@gmail.com>
2023-11-15 10:51:20 +0700

    created: default config for the ASDR

--- configs/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_asdr6_2_100_autosplit.py
+++ configs/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_asdr6_2_100_autosplit.py
@@ -1,7 +1,6 @@
 _base_ = "yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco.py"
 
-# ========================Frequently modified parameters======================
-# -----data related-----
+# data related
 data_root = "data/asdr6_2_100_autosplit/"
 
 train_ann_file = "annotations/train.json"
@@ -89,27 +88,23 @@ palette = [
     (204, 90, 90),
     (204, 204, 204),
 ]
-
 metainfo = dict(classes=class_name, palette=palette)
 
-anchors = [
-    [(68, 69), (154, 91), (143, 162)],  # P3/8
-    [(242, 160), (189, 287), (391, 207)],  # P4/16
-    [(353, 337), (539, 341), (443, 432)],  # P5/32
-]
 
-max_epochs = 40
-train_batch_size_per_gpu = 6
-train_num_workers = 4
-
-load_from = "https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco_20221027_215044-58865c19.pth"  # noqa
-
+# model related
+max_epochs = 300
+train_batch_size_per_gpu = 12
+train_num_workers = 8
+load_from = "https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco_20221027_215044-58865c19.pth"
 model = dict(
     bbox_head=dict(
         head_module=dict(num_classes=num_classes),
     ),
 )
+train_cfg = dict(max_epochs=max_epochs, val_interval=10)
 
+
+# train val related
 train_dataloader = dict(
     batch_size=train_batch_size_per_gpu,
     num_workers=train_num_workers,
@@ -120,7 +115,6 @@ train_dataloader = dict(
         data_prefix=dict(img="images/"),
     ),
 )
-
 val_dataloader = dict(
     dataset=dict(
         metainfo=metainfo,
@@ -129,22 +123,20 @@ val_dataloader = dict(
         data_prefix=dict(img="images/"),
     )
 )
-
 test_dataloader = val_dataloader
-
-_base_.optim_wrapper.optimizer.batch_size_per_gpu = train_batch_size_per_gpu
 
 val_evaluator = dict(ann_file=data_root + "annotations/val.json")
 test_evaluator = val_evaluator
 
+
+# hooks
 default_hooks = dict(
     checkpoint=dict(interval=10, max_keep_ckpts=2, save_best="auto"),
-    # The warmup_mim_iter parameter is critical.
-    # The default value is 1000 which is not suitable for cat datasets.
     param_scheduler=dict(max_epochs=max_epochs, warmup_mim_iter=1000),
     logger=dict(type="LoggerHook", interval=5),
 )
-train_cfg = dict(max_epochs=max_epochs, val_interval=10)
+
+# visualization config
 visualizer = dict(
     vis_backends=[dict(type="LocalVisBackend"), dict(type="WandbVisBackend")]
-)  # noqa
+)
