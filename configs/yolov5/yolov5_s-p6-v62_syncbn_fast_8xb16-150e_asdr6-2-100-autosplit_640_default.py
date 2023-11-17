bc3eeb208d7d39de35493b4d007df89d17cd183d
klinsc <klinscea@gmail.com>
2023-11-17 10:06:11 +0700

    Revert "Update yolov5_s-p6-v62_syncbn_fast_8xb16-150e_asdr6-2-100-autosplit_640_default.py"
    This reverts commit ec59e079b363b1d536e51a43b20152673eb02291.

--- configs/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-150e_asdr6-2-100-autosplit_640_default.py
+++ configs/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-150e_asdr6-2-100-autosplit_640_default.py
@@ -92,8 +92,8 @@ metainfo = dict(classes=class_name, palette=palette)
 
 # model related
 max_epochs = 150
-train_batch_size_per_gpu = 16  # 16/12 does not fit in 15GB
-train_num_workers = 8  # 8/6 does not fit in 15GB
+train_batch_size_per_gpu = 8  # 16/12 does not fit in 15GB
+train_num_workers = 4  # 8/6 does not fit in 15GB
 load_from = "https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco_20221027_215044-58865c19.pth"
 model = dict(
     bbox_head=dict(
