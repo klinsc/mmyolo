f020765c36f7222a9279ee763800da36691bd20c
klinsc <klinscea@gmail.com>
2023-12-22 16:39:14 +0700

    Update yolov5_s-p6-v62_syncbn_fast_8xb16-300e_asdr6-2-250-640-split82-114Padding640_640_anchOptm.py

--- configs/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_asdr6-2-250-640-split82-114Padding640_640_anchOptm.py
+++ configs/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_asdr6-2-250-640-split82-114Padding640_640_anchOptm.py
@@ -109,8 +109,8 @@ batch_shapes_cfg = dict(
 
 # model related
 max_epochs = 300
-train_batch_size_per_gpu = 1  # 16/12 does not fit in 15GB
-train_num_workers = 1  # 8/6 does not fit in 15GB
+train_batch_size_per_gpu = 16  # 16/12 does not fit in 15GB
+train_num_workers = 8  # 8/6 does not fit in 15GB
 load_from = "https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco/yolov5_s-p6-v62_syncbn_fast_8xb16-300e_coco_20221027_215044-58865c19.pth"
 model = dict(
     bbox_head=dict(
